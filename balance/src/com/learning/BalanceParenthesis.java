fba2e754782f6407c85dfeecddf9a8d8c3eb26dc
abhishekkv72 <93667184+abhishekkv72@users.noreply.github.com>
2022-01-31 12:44:34 +0530

    Add files via upload

---
+++ balance/src/com/learning/BalanceParenthesis.java
@@ -0,0 +1,70 @@
+package com.learning;
+
+import java.util.Stack;
+
+public class BalanceParenthesis {
+
+	public static void main(String[] args) {
+		
+		
+		String input = "{[]}()[{}]";
+		
+		boolean result = isParenthesisisBalanced(input);
+		
+		if(result) {
+			System.out.println("The entered string has balanced brackets");
+			
+		}else {
+			System.out.println("The entered string do not contain balanced brackets");
+		}
+		
+
+	}
+
+	private static boolean isParenthesisisBalanced(String input) {
+		
+		Stack<Character> stack = new Stack<Character>();
+		char[] inputArray = input.toCharArray();
+		for(int i = 0; i < inputArray.length; i++) {
+			
+			
+			if(inputArray[i] == '(' || inputArray[i] == '[' || inputArray[i] == '{') {
+				stack.push(inputArray[i]);
+				continue;
+			}
+			if (stack.isEmpty()) {
+				return false;
+			}
+			char c;
+			switch(inputArray[i]) {
+			
+			case ')':
+				c = stack.pop();
+				if (c == ']' || c == '}') {
+					return false;
+				}
+				break;
+			case ']':
+				c = stack.pop();
+				if (c == ')' || c == '}') {
+					return false;
+				}
+				break;
+			case '}':
+				c = stack.pop();
+				if (c == ']' || c == ')') {
+					return false;
+				}
+				break;
+				}
+			}
+		
+		
+		if(stack.isEmpty()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+}
